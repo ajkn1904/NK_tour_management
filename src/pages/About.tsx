7b17e178fcb435636ac9f5a97790a27abe812590
ajkn1904 <ajknishat@gmail.com>
2025-08-14 04:46:13 +0600

    first commit

---
+++ src/pages/About.tsx
@@ -0,0 +1,9 @@
+export default function About() {
+    return (
+        <div>
+        <h1>About Page</h1>
+        <p>This is a tour management project built with shadcn and React.</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi quis eos aut aspernatur placeat vel reprehenderit consequatur totam! Voluptate, tempore a? Dolores voluptatum iusto eos error consequuntur! Architecto, iste dolorum.</p>
+        </div>
+    );
+}
